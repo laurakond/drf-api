394dcea0224451cd44270a9ad0a70c3bd6e864a4
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-11-01 16:18:01 +0000

    add followers views

--- likes/views.py
+++ likes/views.py
@@ -9,8 +9,8 @@ class LikeList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Like.objects.all()
 
-    def perform_create(self, Serializer):
-        Serializer.save(owner=self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class LikeDetail(generics.RetrieveDestroyAPIView):
