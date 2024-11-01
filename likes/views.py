01ef2f5a0c88c7d5f216b21d143804e085da387b
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-11-01 14:20:11 +0000

    add likes app

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, Serializer):
+        Serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly] # only the owner who liked the post can unlike it
+    serializer_class = LikeSerializer # not to have to send  the post id every time to edit a comment
+    queryset = Like.objects.all()
