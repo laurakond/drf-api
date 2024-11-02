88a0d851a4c872e5a3101797a59e17b2f73425ae
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-11-02 09:33:43 +0000

    add filtering and count to profile view

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,6 +7,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    post_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -26,5 +29,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
-            'content', 'images', 'is_owner', 'following_id'
+            'content', 'images', 'is_owner', 'following_id',
+            'post_count', 'followers_count', 'following_count'
         ]
