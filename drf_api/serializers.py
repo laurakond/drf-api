60880d64b1ab721605874b4cd4b37dfe4e4e4e7a
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-11-06 18:30:55 +0000

    change image url name

--- drf_api/serializers.py
+++ drf_api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='profile.images.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
