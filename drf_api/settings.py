a4406b8a47d8b623df7199f0a90e8caa84423ce8
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-11-24 08:29:35 +0000

    revert allowed local host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,6 @@ ALLOWED_HOSTS = [
     '8000-laurakond-drfapi-4hrfoij2t7d.ws.codeinstitute-ide.net',
     '8000-laurakond-drfapi-taq1ruy5aei.ws.codeinstitute-ide.net',
     'localhost',
-    'https://moments-lk-4fd4ad1a172b.herokuapp.com',
     os.environ.get('ALLOWED_HOST'),
     ]
 
